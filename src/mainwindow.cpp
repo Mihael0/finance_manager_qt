991b3b259ae9035c1f8dab86db96b3a2ee29575a
CodeEnjoyer <mihael.nedyalkov@gmail.com>
2025-02-28 17:19:12 +0100

    Clarified the next steps and added a comment about non-POD static

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,6 +2,11 @@
 #include "ui_mainwindow.h"
 #include "timeutils.h"
 
+// non-POD static.
+// Means that there might be a chance that the constructor of this is called before it is declared.
+// It is stated here that it might not be a problem https://stackoverflow.com/questions/1538137/c-static-global-non-pod-theory-and-practice
+// But it is not certain. For now, I am unsure on how to address this without using a Singleton if this is to be removed.
+// Will test it and if the problem does not arise, it will be kept like this. Hopefully not code debt.
 static TimeUtils Time;
 
 MainWindow::MainWindow(QWidget *parent)
@@ -28,9 +33,11 @@ MainWindow::~MainWindow()
 void MainWindow::on_dailyExpenses_returnPressed()
 {
     // Excel takes in the inputted float number.
+    // And we pass the LocalApptime.
+    // We need to have access to the created sheets
+    // Maybe like, fetch currentMonth sheet or something?
 
 }
-
 
 void MainWindow::on_nextDay_clicked()
 {
